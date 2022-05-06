06bf3fdeb498e8285885e103cd7019c244a6e985
Noars <55838787+Noars@users.noreply.github.com>
2022-05-06 11:19:46 +0200

    Update install of gazeplay in cubic

--- ISOScripts/gazeplayInstall.sh
+++ ISOScripts/gazeplayInstall.sh
@@ -7,19 +7,21 @@ NEW_VERSION_LINK=$(echo "$LATEST_RELEASE_INFO" | grep "browser_download_url.*gaz
 
 NEW_VERSION=$( echo ${NEW_VERSION_LINK} | cut -d/ -f9)
 
-NEW_VERSION_NO_EXT=$( echo ${NEW_VERSION} | cut -d. -f1,2,3)
+TAG_VERSION=$(echo "$LATEST_RELEASE_INFO" | grep "name.*GazePlay*" | cut -d: -f2 | tr -d \" | head -n 1 | tr -d \, | cut -d" " -f3)
 
-NEW_VERSION_NAME=$(echo "$LATEST_RELEASE_INFO" | grep "name.*GazePlay*" | cut -d: -f2,3 | tr -d \" | head -n 1 | tr -d \,)
+NEW_VERSION_NO_EXT=$( echo ${NEW_VERSION} | cut -d. -f1,2,3 | cut -d. -f1)
 
-cd /etc/skel || exit
+NEW_NAME="$NEW_VERSION_NO_EXT-$TAG_VERSION"
 
-echo "Download of ${NEW_VERSION_NO_EXT}"
+NEW_VERSION_NAME=$(echo "$LATEST_RELEASE_INFO" | grep "name.*GazePlay*" | cut -d: -f2 | tr -d \" | head -n 1 | tr -d \,)
+
+echo "Download of ${NEW_NAME}"
 
 wget $NEW_VERSION_LINK
 
-tar -zxvf ${NEW_VERSION} >>/etc/skel/log/tarGazePLay.log
+tar -zxvf ${NEW_VERSION}
 
-mv "${NEW_VERSION_NO_EXT}" "${NEW_VERSION_NAME}"
+mv "${NEW_NAME}" "${NEW_VERSION_NAME}"
 
 ls | grep -i "gazeplay-.*" | egrep -v "^(${NEW_VERSION_NAME}$)" | while read -r line; do 
 rm -rf "${line}"; 
