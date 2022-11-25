b9336e882cb992e666c9de9ffa13b32125ce53fb
Noars <55838787+Noars@users.noreply.github.com>
2022-11-25 09:52:24 +0100

    Update interAACtionPlayerInstall.sh

--- ISOScripts/interAACtionPlayerInstall.sh
+++ ISOScripts/interAACtionPlayerInstall.sh
@@ -3,7 +3,7 @@ vert='\e[0;32m'
 
 LATEST_RELEASE_INFO=$(curl -s https://api.github.com/repos/AFSR/InterAACtionPlayer-AFSR/releases/latest)
 
-NEW_VERSION_LINK=$(echo "$LATEST_RELEASE_INFO" | grep "browser_download_url.*InterAACtionPlayer*" | cut -d: -f2,3 | tr -d \")
+NEW_VERSION_LINK=$(echo "$LATEST_RELEASE_INFO" | grep "browser_download_url.*InterAACtionPlayer.tar.gz*" | cut -d: -f2,3 | tr -d \")
 
 NEW_VERSION=$( echo "${NEW_VERSION_LINK}" | cut -d/ -f9)
 
