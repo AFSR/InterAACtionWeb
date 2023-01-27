89f39cdeb5e35a534743bc80b19e6433f896fd5a
Noars <55838787+Noars@users.noreply.github.com>
2023-01-27 11:10:18 +0100

    Update interAACtionBoxInterfaceInstall.sh

--- ISOScripts/interAACtionBoxInterfaceInstall.sh
+++ ISOScripts/interAACtionBoxInterfaceInstall.sh
@@ -9,7 +9,7 @@ NEW_VERSION=$( echo "${NEW_VERSION_LINK}" | cut -d/ -f9)
 
 NEW_VERSION_NAME=$(echo "$LATEST_RELEASE_INFO" | grep "name.*InterAACtionBox_Interface-linux*" | cut -d: -f2,3 | tr -d \" | head -n 1 | tr -d \,)
 
-cd ~/ || exit
+cd /etc/skel/ || exit
 
 echo "Download of ${NEW_VERSION_NAME}"
 
@@ -17,9 +17,8 @@ wget $NEW_VERSION_LINK
 
 tar -zxvf "${NEW_VERSION}" >>/etc/skel/log/tarInterAACtionBoxInterface.log
 
-rm -r "${NEW_VERSION}"
+rm *.tar.gz
 
-cp -r ~/InterAACtionBox_Interface-linux /etc/skel/
 cd /etc/skel/InterAACtionBox_Interface-linux/lib/jre/bin/
 chmod +x java
 cd /etc/skel/InterAACtionBox_Interface-linux/bin/
